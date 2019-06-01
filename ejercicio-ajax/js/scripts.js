db140f9d4307c87ae6025da9f3f743a3a5b55291
joni <288josue@gamil.com>
2019-06-01 15:48:48 -0600

    update1

--- ejercicio-ajax/js/scripts.js
+++ ejercicio-ajax/js/scripts.js
@@ -1,9 +1,14 @@
 
   $(document).ready(function(){});
 
+  /*----------------------------------------------------------------*/
+
   $("#mostrar").click(function(){
       getProductos();
+      $("#mostrar").css({"display" : "none"});
   });
+
+/*--------------------------------------------------------------*/
 
   function getProductos(){
 
@@ -22,9 +27,8 @@
 
             listaProductos.append(
               "<div>"
-              +"<p>"
               +"<h2>"
-              + pro.name +"-" + pro.price
+              + pro.name+" - "+"$"+ pro.price
               +"</h2>"
               +"<br>"
               +"<div class='md'>"
@@ -32,15 +36,62 @@
               +"</div>"
               +"<br>"
               +"<div class='md'>"
+              +"<p>" + "<strong>Descripción :</strong>" + "<br/><br/>"
               + pro.description
-              +"</div>"
               +"</p>"
+              +"</div>"
               +"</div>"
            );
          }
       },
       error : function(respuesta){
         alert("Error al encontrar y mostrar los productos");
+      },
+    });
+
+  }
+
+/*-----------------------------------------------------------------------*/
+
+    window.onload =  function(){
+
+    $.ajax({
+      type : "GET",
+      url : "https://mini-shop-rg.herokuapp.com/products",
+      dataType : "json",
+
+      success : function(resultado){
+            var listaProductos = $('#ofertas');
+            var collectionsProductos = resultado;
+
+            for (var i = 0; i < collectionsProductos.length; i++) {
+
+            var pro =  collectionsProductos[i]
+
+            if (pro.price < 25.00) {
+              listaProductos.append(
+                "<div class='col-3'>"
+                +"<h2>"
+                + pro.name+" - "+"$"+ pro.price
+                +"</h2>"
+                +"<br>"
+                +"<div class='md2'>"
+                +"<img src="+pro.image+">"
+                +"</div>"
+                +"<br>"
+                +"<div class='md2'>"
+                +"<p>" + "<strong>Descripción :</strong>" + "<br/><br/>"
+                + pro.description
+                +"</p>"
+                +"</div>"
+                +"</div>"
+             );
+            }
+        }
+
+      },
+      error : function(respuesta){
+        alert("Error al encontrar y mostrar los productos en oferta");
       },
     });
 
